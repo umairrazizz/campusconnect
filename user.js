fa2d0280c8d0d3264247bc56ef30dedbbc1062f9
umair aziz <umairazizdar@gmail.com>
2024-10-14 22:47:16 +0530

    Delete user.js

--- user.js
+++
@@ -1,8 +0,0 @@
-// users.js
-const users = []; // In-memory user storage for demonstration
-
-module.exports = {
-  findUser: (username) => users.find(user => user.username === username),
-  addUser: (user) => users.push(user),
-  getAllUsers: () => users,
-};
