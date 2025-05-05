00cf992c422f8d01398424dce86996295850305a
umair aziz <umairazizdar@gmail.com>
2025-05-05 14:39:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,3 +31,89 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Info Page</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .navbar {
+            background-color: #eee;
+            padding: 10px;
+        }
+        .navbar a {
+            margin-right: 20px;
+            text-decoration: none;
+            color: black;
+            font-weight: bold;
+        }
+        .container {
+            display: flex;
+            justify-content: space-around;
+            margin-top: 20px;
+        }
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 8px;
+        }
+        form {
+            display: flex;
+            flex-direction: column;
+            width: 200px;
+        }
+        form input {
+            margin-bottom: 10px;
+            padding: 5px;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="navbar">
+        <a href="#">Home</a>
+        <a href="#">About us</a>
+        <a href="#">Blog</a>
+    </div>
+
+    <div class="container">
+        <div>
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Roll No</th>
+                </tr>
+                <tr>
+                    <td>John</td>
+                    <td>101</td>
+                </tr>
+                <tr>
+                    <td>Jane</td>
+                    <td>102</td>
+                </tr>
+            </table>
+        </div>
+
+        <div>
+            <form>
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name">
+                
+                <label for="phone">Phone:</label>
+                <input type="text" id="phone" name="phone">
+
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+    </div>
+
+</body>
+</html>
+
